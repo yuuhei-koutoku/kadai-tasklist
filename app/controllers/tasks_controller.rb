19d6758e5f0522138b18d9fb18c0ebefdbd7e884
yuuhei.koutoku <csyu15095@g.nihon-u.ac.jp>
2020-08-18 08:00:11 +0000

    add column 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,9 +48,13 @@ class TasksController < ApplicationController
   end
   
   private
-
+  
   # Strong Parameter
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
